ed8d8f55f46e50fb33b0ad645625f61b12779ffb
unknown <beating_heart@yeah.net>
2022-10-24 11:46:03 +0800

    fundamental work

---
+++ data/initial.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+REGION_NUM = 2  # region number in the system
+CAR_NUM = 2000  # total car in the system.
+
+# CAR_INITIAL_DISTRIBUTION = 
---
+++ src/empty-car-routing/main.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == "__main__":
+    print("123456")
+
+
+# https://cloud.tencent.com/developer/article/1642626 泊松过程的模拟
