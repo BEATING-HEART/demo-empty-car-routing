7faa6f89d884e90f0ad0f58330574518acc0743e
BEATING-HEART <beating_heart@yeah.net>
2022-10-28 01:06:58 +0800

    add fundamental initial data

--- data/initial.py
+++ data/initial.py
@@ -14,5 +14,20 @@ CAR_DISTRIBUTION_INITIAL = random_allocation(CAR_NUM, REGION_NUM)
 """car distribucation (initial)"""
 
 TRAVEL_MATRIX_P = np.random.dirichlet(np.ones((REGION_NUM)), size=REGION_NUM)
-"""travel probability matrix of the system."""
+"""size=[r region * r region], travel probability matrix of the system."""
 
+PASSENGER_ARRIVIAL_LAMBDA = np.array([800, 400])
+"""size=[1 * r region], corresponding to the lambda of each region."""
+
+TRAVEL_TIME_MU = np.ones((REGION_NUM, REGION_NUM)) 
+"""size=[r region * r region], corresponding to the parameter mu."""
+
+EMPTY_CAR_ROUTING_MATRIX_Q = np.array([[0, 0],[0.5, 0.5]])
+"""size=[r region * r region], empty car routing matrix of the system."""
+
+TIME_INTERVAL = 500
+"""system total running time. (epoch)"""
+
+ORIGINAL_MATRIX_E = np.diag(CAR_DISTRIBUTION_INITIAL)
+
+ORIGINAL_MATRIX_F = np.zeros((REGION_NUM, REGION_NUM))
--- src/empty-car-routing/main.py
+++ src/empty-car-routing/main.py
@@ -1,11 +1,12 @@
 import os, sys
 sys.path.append(os.getcwd())    # 添加根目录为path
 
-from data.initial import CAR_DISTRIBUTION_INITIAL, TRAVEL_MATRIX_P
+from data.initial import CAR_DISTRIBUTION_INITIAL, TRAVEL_MATRIX_P, ORIGINAL_MATRIX_E
 
 if __name__ == "__main__":
     print(CAR_DISTRIBUTION_INITIAL)
     print(TRAVEL_MATRIX_P)
+    print(ORIGINAL_MATRIX_E)
 
 
 # https://cloud.tencent.com/developer/article/1642626 泊松过程的模拟
