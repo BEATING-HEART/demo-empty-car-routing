0386f7c5e3ce0af9897f4e98b009587567e129c5
unknown <beating_heart@yeah.net>
2022-10-27 17:29:51 +0800

    fundamental

--- data/initial.py
+++ data/initial.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import SIZE
 import numpy as np
 
 from utils.utils import random_allocation
@@ -9,4 +10,9 @@ CAR_NUM = 1200
 """total car in the system"""
 
 CAR_DISTRIBUTION_INITIAL = random_allocation(CAR_NUM, REGION_NUM)
+# CAR_DISTRIBUTION_INITIAL = np.random.dirichlet(np.ones(REGION_NUM), size=1)
 """car distribucation (initial)"""
+
+TRAVEL_MATRIX_P = np.random.dirichlet(np.ones((REGION_NUM)), size=REGION_NUM)
+"""travel probability matrix of the system."""
+
--- src/empty-car-routing/main.py
+++ src/empty-car-routing/main.py
@@ -1,10 +1,11 @@
 import os, sys
 sys.path.append(os.getcwd())    # 添加根目录为path
 
-from data.initial import CAR_DISTRIBUTION_INITIAL
+from data.initial import CAR_DISTRIBUTION_INITIAL, TRAVEL_MATRIX_P
 
 if __name__ == "__main__":
     print(CAR_DISTRIBUTION_INITIAL)
+    print(TRAVEL_MATRIX_P)
 
 
 # https://cloud.tencent.com/developer/article/1642626 泊松过程的模拟
