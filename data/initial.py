a5c3a5c5e684c365b21e7c3f0935f7086a841bc0
unknown <beating_heart@yeah.net>
2022-10-28 16:45:02 +0800

    fundamental settings

--- data/initial.py
+++ data/initial.py
@@ -19,15 +19,26 @@ TRAVEL_MATRIX_P = np.random.dirichlet(np.ones((REGION_NUM)), size=REGION_NUM)
 PASSENGER_ARRIVIAL_LAMBDA = np.array([800, 400])
 """size=[1 * r region], corresponding to the lambda of each region."""
 
+PASSENGER_INITIAL = np.random.poisson(PASSENGER_ARRIVIAL_LAMBDA)
+
 TRAVEL_TIME_MU = np.ones((REGION_NUM, REGION_NUM)) 
 """size=[r region * r region], corresponding to the parameter mu."""
 
-EMPTY_CAR_ROUTING_MATRIX_Q = np.array([[0, 0],[0.5, 0.5]])
+EMPTY_CAR_ROUTING_MATRIX_Q = np.array([[0, 0],[0.5, 0]])
 """size=[r region * r region], empty car routing matrix of the system."""
 
 TIME_INTERVAL = 500
 """system total running time. (epoch)"""
 
 ORIGINAL_MATRIX_E = np.diag(CAR_DISTRIBUTION_INITIAL)
+"""original empty car matrix. (all cars are empty)"""
 
 ORIGINAL_MATRIX_F = np.zeros((REGION_NUM, REGION_NUM))
+"""original full car matrix. (no car is full)"""
+
+ORIGINAL_AVAILABILITY = np.clip(np.divide(CAR_DISTRIBUTION_INITIAL, PASSENGER_INITIAL), 0, 1)
+"""original car availability."""
+
+# ORIGINAL_MATRIX_F_MEAN = ORIGINAL_MATRIX_F / CAR_NUM
+
+# ORIGINAL_MATRIX_E_MEAN = ORIGINAL_MATRIX_E / CAR_NUM
