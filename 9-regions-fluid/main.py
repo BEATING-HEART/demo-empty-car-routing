38e0da758b5841f773c9ae039327f5344d5be321
BEATING-HEART <beating_heart@yeah.net>
2022-11-28 12:33:00 +0800

    9-region-initial

---
+++ 9-regions-fluid/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from parameters import (
+    TRAVEL_MATRIX_P,
+    RECIPROCAL_MU_MATRIX
+)
+
+
+
+if __name__ == "__main__":
+    # print(TRAVEL_MATRIX_P)
+    # print(RECIPROCAL_MU_MATRIX)
+    # mu_matrix = np.reciprocal(RECIPROCAL_MU_MATRIX)
+    # print(mu_matrix)
+    
+    pass
