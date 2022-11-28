0830b02564d00d72f1028ec5a136118c160d7dcb
BEATING-HEART <beating_heart@yeah.net>
2022-11-29 01:11:19 +0800

    update main.py

--- 9-regions-fluid/main.py
+++ 9-regions-fluid/main.py
@@ -1,16 +1,28 @@
+import cvxpy as cp
 import numpy as np
 
 from parameters import (
     TRAVEL_MATRIX_P,
-    RECIPROCAL_MU_MATRIX
+    RECIPROCAL_MU_MATRIX,
+    CUSTOMER_ARRIVIAL_LAMBDA
 )
 
 
 
 if __name__ == "__main__":
-    # print(TRAVEL_MATRIX_P)
-    # print(RECIPROCAL_MU_MATRIX)
-    # mu_matrix = np.reciprocal(RECIPROCAL_MU_MATRIX)
-    # print(mu_matrix)
     
-    pass
+    p_matrix = cp.Parameter((9, 9), nonneg=True, value=TRAVEL_MATRIX_P)  # ok
+    
+    lambda_arrival = cp.Parameter(9, nonneg=True, value=CUSTOMER_ARRIVIAL_LAMBDA)   # ok
+
+    mu_matrix = cp.Parameter((9, 9), nonneg=True, value=np.reciprocal(RECIPROCAL_MU_MATRIX))    # ok
+    
+    
+    
+    print(p_matrix.value[0])
+    # print((p_matrix @ p_matrix).value)
+    
+    # print(RECIPROCAL_MU_MATRIX)
+    # print(mu_matrix.value)
+    
+    # pass
